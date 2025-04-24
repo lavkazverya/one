2a1145508505524ca014fa6584d593701a4ddac3
rick <rick.broker@gmail.com>
2025-04-24 14:15:14 -0400

    add echo page

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,36 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
+function EchoPage() {
+  return(
+    <>
+    <h1>Echo Page</h1>
+    <hr/>
+    <a href="/"><button >Home</button></a>
+    </>
+  );
+}
+
 function HomePage() {
-  return <h1>Welcome to the Demo, Rick</h1>
+  return(
+    <>
+    <div className="App">
+       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       </header>
+       <h1>Welcome to the Demo, Rick</h1>
+       <a href="/echo"><button >Echo</button></a>
+    </div>
+    </>
+  );
 }
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //   </header>
-    // </div>
     <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/echo" element={<EchoPage />} />
     </Routes>
   </Router>
   );
