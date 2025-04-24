4de5ed4767d62acebde441e3066c2897309ba93f
rick <rick.broker@gmail.com>
2025-04-24 14:25:58 -0400

    update tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Demo title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Demo/i);
   expect(linkElement).toBeInTheDocument();
 });
